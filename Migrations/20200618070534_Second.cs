5c886df53dd78337c0d088865f95481b75ee30c7
Monalisa-Andreea <56880417+Monalisa-Andreea@users.noreply.github.com>
2021-11-04 19:42:14 +0200

    Add files via upload

---
+++ Migrations/20200618070534_Second.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartGarden.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "WaterLevelSensors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "TemperatureSensors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "SoilMoistureSensors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "LightSensors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CurrentDate",
+                table: "BarometricSensors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "WaterLevelSensors");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "TemperatureSensors");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "SoilMoistureSensors");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "LightSensors");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentDate",
+                table: "BarometricSensors");
+        }
+    }
+}
